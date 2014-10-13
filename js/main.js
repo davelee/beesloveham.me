f58ca0068c00ba2ccbe30cb5e9660c3d7196191e
Dave Lee <davmlee@umich.edu>
2014-10-12 23:33:44 -0400

    jquery and shnaz and bringing your story back

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 function goToByScroll(id){
 	$('html,body').animate({scrollTop: $("#"+id).offset().top},'slow');
 }
+
+$(document).ready(function () {
+	console.log("blah");
+	$(".slide-box").click(
+		function (event) {
+			var strId = $(this).attr("id");
+			var idn = strId.split("slide")[1];
+			var intIdn = parseInt(idn);
+			var next_id = "slide" + (intIdn+1).toString();
+			goToByScroll(next_id);
+	});
+});
+
+
+
