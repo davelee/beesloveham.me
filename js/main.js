d8afbe37db387bacc73d7e7ef2ccc1dde0dbe7e3
Dave Lee <davmlee@umich.edu>
2014-10-13 13:51:32 -0400

    robustier

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
+var numSlides = 3;
+
 function goToByScroll(id){
 	$('html,body').animate({scrollTop: $("#"+id).offset().top},'slow');
 }
 
 $(document).ready(function () {
-	console.log("blah");
 	$(".slide-box").click(
 		function (event) {
 			var strId = $(this).attr("id");
 			var idn = strId.split("slide")[1];
 			var intIdn = parseInt(idn);
-			var next_id = "slide" + (intIdn+1).toString();
+			intIdn = (intIdn == numSlides) ? 1 : intIdn+1;
+			var next_id = "slide" + intIdn.toString();
 			goToByScroll(next_id);
 	});
 });
